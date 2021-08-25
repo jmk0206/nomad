185a58fd33e216bcb9212c19f12d27a5a7a670de
jmk0206 <jmk960206@gmail.com>
2021-08-25 20:09:57 +0900

    아이디 저장 및 로딩

--- js/app.js
+++ js/app.js
@@ -3,21 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(event) {
 	event.preventDefault();
 	loginForm.classList.add("hidden")
 	const username = loginInput.value;
-	// greeting.innerText ="Hello " + username;
+	localStorage.setItem(USERNAME_KEY, username);
+	paintGreetings();
+}
+
+function paintGreetings(username) {
 	greeting.innerText = `Hello ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-	event.preventDefault();
-	console.dir(event);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+	paintGreetings(savedUsername);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
-
