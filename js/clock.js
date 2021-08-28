db70394cf2ada5bf7ab97b72b50d293dff91bfcd
jmk0206 <jmk960206@gmail.com>
2021-08-28 12:48:04 +0900

    수정

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const clock	= document.querySelector("h2#clock");
 
 function getClock() {
@@ -10,17 +9,4 @@ function getClock() {
 }
 
 getClock()
-=======
-const clock	= document.querySelector("h2#clock");
-
-function getClock() {
-	const date = new Date();
-	const hours = String(date.getHours()).padStart(2, "0");
-	const minutes = String(date.getMinutes()).padStart(2, "0");
-	const seconds = String(date.getSeconds()).padStart(2, "0");
-	clock.innerText = `${hours}:${minutes}:${seconds}`; // 백틱
-}
-
-getClock()
->>>>>>> 307de8bea3b99fad733c92dee496279326700175
 setInterval(getClock, 1000);
