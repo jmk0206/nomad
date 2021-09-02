a95d70571a0618ebf6f9d7224d547a770864a344
jmk0206 <jmk960206@gmail.com>
2021-09-02 20:35:09 +0900

    이후에는 기본적은 구조는 다 갖췄으니 원하는 방향으로 꾸며보자

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "a9412c66cf3df1313ec94f391f6e8627";
+
 function onGeoOk(position) {
 	const lat = position.coords.latitude;
-	const lng = position.coords.longitude;
-	console.log("You live it", lat, lng);
+	const lon = position.coords.longitude;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			const weather = document.querySelector("#weather span:first-child");
+			const city = document.querySelector("#weather span:last-child");
+			weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+			city.innerText = data.name;
+		}); // URL을 fetch하고 그 다음으로 response를 받는다.
 }
 
 function onGeoError() {
