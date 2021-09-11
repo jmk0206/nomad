4ae3cba69a8c6263c69809ec06d21a54f1270430
jmk0206 <jmk960206@gmail.com>
2021-09-11 20:59:38 +0900

    css 수정

--- js/greetings.js
+++ js/greetings.js
@@ -5,7 +5,6 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-
 function onLoginSubmit(event) {
 	event.preventDefault();
 	loginForm.classList.add("hidden")
@@ -15,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-	greeting.innerText = `Hello ${username}`;
+	greeting.innerText = `Hello, ${username}`;
 	greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
